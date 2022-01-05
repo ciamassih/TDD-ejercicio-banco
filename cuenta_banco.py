d19d41aad1a1d537cbec59adccf24057e0fae280
ciamassih <c.abdulmassihlopez1@usp.ceu.es>
2022-01-05 18:29:27 +0100

    test cuenta con dinero

--- cuenta_banco.py
+++ cuenta_banco.py
@@ -2,3 +2,6 @@ class Cuenta():
     def estado (self):
         estado = 0
         return estado
+
+    def sumar(num1, num2):
+        return num1 + num2
--- test_banco.py
+++ test_banco.py
@@ -8,3 +8,12 @@ class TestCuentaBanco(unittest.TestCase):
     def test_cuenta_vacía(self):
         estado = self.cuenta.estado()
         self.assertEqual(estado, 0)
+
+    def test_cuenta_de_pobre(self):
+        suma = Cuenta.sumar(0, 5)
+        self.assertEqual(suma, 5)
+
+    def test_cuenta_no_tan_pobre(self):
+        suma = Cuenta.sumar(7, 5)
+        self.assertEqual(suma, 12)
+
