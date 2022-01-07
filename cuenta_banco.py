340c6c9fe981b06c6aa5fb38c55cc28a7a33f0fd
ciamassih <c.abdulmassihlopez1@usp.ceu.es>
2022-01-07 18:00:21 +0100

    test cuenta vacia con ingreso fallando

--- cuenta_banco.py
+++ cuenta_banco.py
@@ -1,4 +1,7 @@
 class Cuenta:
 
     def __init__(self, balance):
-        self.balance = 0
+        self.balance = balance
+
+    def ingreso(self, cantidad):
+        pass
--- test_banco.py
+++ test_banco.py
@@ -8,5 +8,14 @@ class TestCuentaBanco(unittest.TestCase):
         cuenta = Cuenta(0)
         self.assertEqual(cuenta.balance, 0)
 
+    def test_balance_1(self):
+        cuenta = Cuenta(1)
+        self.assertEqual(cuenta.balance, 1)
+
+    def test_ingreso_cuenta_0(self):
+        cuenta = Cuenta(0)
+        cuenta.ingreso(1)
+        self.assertEqual(cuenta.balance, 1)
+
 
 
